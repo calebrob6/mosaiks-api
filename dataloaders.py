cada42b6bd9b91877b4176d111e4769eeda74cec
Caleb Robinson <calebrob6@gmail.com>
2021-08-16 19:23:03 +0000

    Adding batched vs single endpoints for NAIP

---
+++ dataloaders.py
@@ -0,0 +1,38 @@
+import rasterio
+import rasterio.warp
+import rasterio.mask
+import shapely.geometry
+
+import torch
+from torch.utils.data import Dataset
+
+class CustomNAIPDataset(Dataset):
+
+    def __init__(self, points, fns, buffer=250):
+        self.points = points
+        self.fns = fns
+        self.buffer = buffer
+
+    def __len__(self):
+        return self.points.shape[0]
+
+    def __getitem__(self, idx):
+
+        lon, lat = self.points[idx]
+        fn = self.fns[idx]
+
+        point_geom = shapely.geometry.mapping(
+            shapely.geometry.Point(lon, lat)
+        )
+
+        with rasterio.Env():
+            with rasterio.open(fn, "r") as f:
+                point_geom = rasterio.warp.transform_geom("epsg:4326", f.crs.to_string(), point_geom)
+                point_shape = shapely.geometry.shape(point_geom)
+                mask_shape = point_shape.buffer(self.buffer).envelope
+                mask_geom = shapely.geometry.mapping(mask_shape)
+                out_image, _ = rasterio.mask.mask(f, [mask_geom], crop=True)
+
+        out_image = (out_image / 255.0)
+        out_image = torch.from_numpy(out_image).float()
+        return out_image
--- server.py
+++ server.py
@@ -12,7 +12,9 @@ RASTERIO_BEST_PRACTICES = dict(
 )
 os.environ.update(RASTERIO_BEST_PRACTICES)
 
+import numpy as np
 import torch
+from torch.utils.data import DataLoader
 import bottle
 
 import rasterio
@@ -20,6 +22,7 @@ import rasterio.warp
 import rasterio.mask
 import shapely.geometry
 
+from dataloaders import CustomNAIPDataset
 from models import RCF, featurize
 from NAIPTileIndex import NAIPTileIndex
 
@@ -29,6 +32,7 @@ BUFFER_DISTANCE = 250  # in meters
 NUM_FEATURES = 1024
 MODEL = RCF(NUM_FEATURES).eval().to(DEVICE)
 
+MAX_BATCH_SIZE = 1000
 
 def enable_cors():
     """From https://gist.github.com/richard-flosi/3789163
@@ -50,7 +54,85 @@ def do_options():
     return
 
 
-def featurize_single():
+def featurize_naip_batched():
+    bottle.response.content_type = "application/json"
+    data = bottle.request.json
+
+    if "latitudes" not in data:
+        bottle.response.status = 500
+        return json.dumps({
+            "message": "'latitudes' is a required parameter but wasn't sent"}
+        )
+    if "longitudes" not in data:
+        bottle.response.status = 500
+        return json.dumps({
+            "message": "'longitudes' is a required parameter but wasn't sent"}
+        )
+    if len(data["latitudes"]) != len(data["longitudes"]):
+        bottle.response.status = 500
+        return json.dumps({
+            "message": "The 'latitudes' and 'longitudes' inputs are not the same length"}
+        )
+    if len(data["latitudes"]) > MAX_BATCH_SIZE:
+        bottle.response.status = 500
+        return json.dumps({
+            "message": f"The maximum number of points you can process at once is {MAX_BATCH_SIZE}"}
+        )
+
+
+    points = []
+    for i in range(len(data["latitudes"])):
+        points.append(
+            (data["longitudes"][i], data["latitudes"][i])
+        )
+    points = np.array(points)
+
+    all_fns = []
+    for i, (lon, lat) in enumerate(points):
+        try:
+            fns = list(set(INDEX.lookup_point(lat, lon)))
+        except IndexError as e:
+            bottle.response.status = 500
+            return json.dumps({
+                "message": str(e) + f" on point ({lon}, {lat})"}
+            )
+        fns = sorted(fns, key=lambda x: int(x.split("/")[6])) # sort by year
+        fn = fns[-1]
+        all_fns.append(fn)
+
+
+    dataset = CustomNAIPDataset(points, all_fns, buffer=BUFFER_DISTANCE)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=8,
+        shuffle=False,
+        num_workers=8,
+        collate_fn=lambda x: x,
+        pin_memory=False
+    )
+
+    x_all = np.zeros((points.shape[0], NUM_FEATURES), dtype=float)
+    i = 0
+    for images in dataloader: 
+        for image in images:
+            if image.shape[0] == 4:
+                image = image.to(DEVICE)
+                with torch.no_grad():
+                    feats = MODEL(image.unsqueeze(0)).cpu().numpy()
+                x_all[i] = feats
+            elif image.shape[0] == 3:
+                pass # this happens in at least Nevada for some points
+            i += 1
+
+    data["features"] = x_all.tolist()
+
+    del dataset, dataloader
+
+    bottle.response.status = 200
+    return json.dumps(data)
+
+
+def featurize_naip_single():
     bottle.response.content_type = "application/json"
     data = bottle.request.json
 
@@ -137,8 +219,11 @@ def main():
 
     app.add_hook("after_request", enable_cors)
 
-    app.route("/featurizeSingle", method="OPTIONS", callback=do_options)
-    app.route("/featurizeSingle", method="POST", callback=featurize_single)
+    app.route("/featurizeNAIPSingle", method="OPTIONS", callback=do_options)
+    app.route("/featurizeNAIPSingle", method="POST", callback=featurize_naip_single)
+
+    app.route("/featurizeNAIPBatched", method="OPTIONS", callback=do_options)
+    app.route("/featurizeNAIPBatched", method="POST", callback=featurize_naip_batched)
 
     bottle_server_kwargs = {
         "host": args.host,
